5b0146aa141fd3f96ca8782a28d6a1131e6eca7d
LindsayM23 <lindsay.soldevilladelgado@griffithuni.edu.au>
2024-10-02 23:34:57 +1000

    filter based on level incomplete

--- run.py
+++ run.py
@@ -54,6 +54,7 @@ class CalcFrame(MyFrame1):
     def OnSearch(self, event):
         keyword = self.m_textCtrl1.GetValue()
         nutrient_name = self.m_textCtrl2.GetValue().strip()
+        nutrient_level = self.m_radioBox1.GetStringSelection().strip()
 
         df = self.table.data
 
@@ -62,26 +63,31 @@ class CalcFrame(MyFrame1):
             loc = search_data(df, keyword)
             df = df[loc]
         if nutrient_name in df.columns:
-            try:
-                min_value = float(self.m_textCtrl3.GetValue())
-                max_value = float(self.m_textCtrl4.GetValue())
-            except ValueError:
-                wx.MessageBox("Please enter valid numeric values for the range.", "Input Error", wx.OK | wx.ICON_ERROR)
-                return
-
-            #filter based on nutrient
+            min_value = float(self.m_textCtrl3.GetValue())
+            max_value = float(self.m_textCtrl4.GetValue())
+            # filter based on nutrient
             nutrient = df[nutrient_name]
-            loc = (nutrient>=min_value) & (nutrient<=max_value)
-            df = df.loc[loc]
+            range_filter = (nutrient >= min_value) & (nutrient <= max_value)
+            df = df.loc[range_filter]
 
+        #by level
+        if nutrient_name in df.columns:
+            nutrient_max = df[nutrient_name].max()
+            low_threshold = nutrient_max * 0.33
+            mid_threshold = nutrient_max * 0.66
+
+            if nutrient_level == 'Low':
+                loc = df[nutrient_name] < low_threshold
+            elif nutrient_level == 'Mid':
+                loc = (df[nutrient_name] >= low_threshold) & (df[nutrient_name] <= mid_threshold)
+            elif nutrient_level == 'High':
+                loc = df[nutrient_name] > mid_threshold
+            df = df.loc[loc]
         if not df.empty:
             tabl = DataTable(df)
             self.m_grid1.ClearGrid()
             self.m_grid1.SetTable(tabl, takeOwnership=True)
             self.m_grid1.AutoSize()
-        else:
-            wx.MessageBox(f"No data found for '{nutrient_name}' within the given range.", "No Results",
-                          wx.OK | wx.ICON_INFORMATION)
 
 if __name__ == "__main__":
     app = wx.App()
